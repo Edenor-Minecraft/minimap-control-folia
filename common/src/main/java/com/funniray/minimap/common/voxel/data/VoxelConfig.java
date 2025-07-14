95ed4869325edfb089405eaf64ae747c956f991b
funniray <funniray10@gmail.com>
2025-07-14 16:04:03 +0100

    feat: allow overriding xaeros/voxel configs via permissions (#18)

--- common/src/main/java/com/funniray/minimap/common/voxel/data/VoxelConfig.java
+++ common/src/main/java/com/funniray/minimap/common/voxel/data/VoxelConfig.java
@@ -1,5 +1,6 @@
 package com.funniray.minimap.common.voxel.data;
 
+import com.funniray.minimap.common.api.MinimapPlayer;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @ConfigSerializable
@@ -9,4 +10,24 @@ public class VoxelConfig {
     public boolean radarPlayersAllowed = true;
     public boolean cavesAllowed = true;
     public String teleportCommand = "tp %p %x %y %z";
+
+    private boolean getOverride(String nodeSuffix, boolean def, MinimapPlayer player) {
+        if (player.hasPermission("minimap.override."+nodeSuffix+".enabled")) {
+            return true;
+        } else if (player.hasPermission("minimap.override."+nodeSuffix+".disabled")) {
+            return false;
+        }
+
+        return def;
+    }
+
+    public VoxelConfig applyOverrides(MinimapPlayer player) {
+        VoxelConfig newConfig = new VoxelConfig();
+        newConfig.radarAllowed = getOverride("radar", this.radarAllowed, player);
+        newConfig.radarMobsAllowed = getOverride("radar-mobs", this.radarMobsAllowed, player);
+        newConfig.radarPlayersAllowed = getOverride("radar-players", this.radarPlayersAllowed, player);
+        newConfig.cavesAllowed = getOverride("cave-mode", this.cavesAllowed, player);
+
+        return newConfig;
+    }
 }
--- common/src/main/java/com/funniray/minimap/common/xaeros/XaerosConfig.java
+++ common/src/main/java/com/funniray/minimap/common/xaeros/XaerosConfig.java
@@ -1,5 +1,6 @@
 package com.funniray.minimap.common.xaeros;
 
+import com.funniray.minimap.common.api.MinimapPlayer;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @ConfigSerializable
@@ -7,4 +8,23 @@ public class XaerosConfig {
     public boolean caveMode = true;
     public boolean netherCaveMode = true;
     public boolean radar = true;
+
+    private boolean getOverride(String nodeSuffix, boolean def, MinimapPlayer player) {
+        if (player.hasPermission("minimap.override."+nodeSuffix+".enabled")) {
+            return true;
+        } else if (player.hasPermission("minimap.override."+nodeSuffix+".disabled")) {
+            return false;
+        }
+
+        return def;
+    }
+
+    public XaerosConfig applyOverrides(MinimapPlayer player) {
+        XaerosConfig newConfig = new XaerosConfig();
+        newConfig.radar = getOverride("radar", this.radar, player);
+        newConfig.caveMode = getOverride("cave-mode", this.caveMode, player);
+        newConfig.netherCaveMode = getOverride("nether-cave-mode", this.netherCaveMode, player);
+
+        return newConfig;
+    }
 }
